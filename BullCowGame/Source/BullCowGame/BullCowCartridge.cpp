7ef52aba487084201bb14f65fc1656a2d0d64ef1
aarbypls <gross614@umn.edu>
2021-02-20 08:29:32 -0600

    Completed IsIsogram method and added HiddenWordList.txt to draw from for guessing

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,9 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    const FString WordListPath = FPaths::ProjectContentDir() / TEXT("WordLists/HiddenWordList.txt");
+    FFileHelper::LoadFileToStringArray(Words, *WordListPath);
 
     SetupGame();
 
@@ -56,6 +60,8 @@ void UBullCowCartridge::EndGame()
 
 void UBullCowCartridge::ProcessGuess(FString Guess)
 {
+    // TO DO: validate hidden word IF they don't add it in
+
     if (Guess == HiddenWord)
     {
         ClearScreen();
@@ -63,24 +69,20 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         return;
     }
 
-    // check if right number of letters
-    // Prompt to guess again if wrong # of letters
     if (Guess.Len() != HiddenWord.Len())
     {
         PrintLine(TEXT("The word has %i letters.\nTry guessing again."), HiddenWord.Len());
         return;    
     }
 
-    // check if isogram
-    // Prompt to guess again if not isogram   
-    // if (!IsIsogram)
-    // {
-    //     PrintLine(TEXT("The word has no repeating letters.\nTry guessing again."));
-    //     return;
-    // }
+    if (!IsIsogram(Guess))
+    {
+        PrintLine(TEXT("Your guess was not an isogram.\nThe word has no repeating letters.\nTry guessing again."));
+        return;
+    }
 
-    // remove a life, show lives left
-    PrintLine(TEXT("You lost a life!"));
+    // Incorrect guess; remove a life
+    PrintLine(TEXT("That guess was incorrect. You lost a life!"));
     PrintLine(TEXT("%i lives remaining."), --Lives);
 
     if (Lives <= 0)
@@ -91,4 +93,27 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         // Play again or quit    
         // Prompt PlayAgain, Press Enter -> PlayAgain?
     }
+}
+
+bool UBullCowCartridge::IsIsogram(FString Word) const
+{
+    // One letter words are by definition isograms   
+    if (Word.Len() == 1)
+    {
+        return true;
+    }
+
+    // Iterate through, checking each letter against the others for duplicates
+    for (int32 Index = 0; Index < Word.Len() - 1; Index++)
+    {
+        for (int32 Comparison = Index + 1; Comparison < Word.Len(); Comparison++)
+        {
+            if (Word[Index] == Word[Comparison])
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
